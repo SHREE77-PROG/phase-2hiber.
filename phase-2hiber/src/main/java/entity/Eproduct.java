3c085479929ca923ec72a4e93a0e3794332511d3
shridhar <shridhar0097@gmail.com>
2022-10-20 14:10:09 +0530

    new message

---
+++ phase-2hiber/src/main/java/entity/Eproduct.java
@@ -0,0 +1,40 @@
+package entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="eproduct_121")
+public class Eproduct {
+	@Id
+	@GeneratedValue
+	@Column(name="eproduct_id")
+	private int id;
+	@Column(name="eproduct_name")
+	private String name;
+	@Column(name="eproduct_price")
+	private double price;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
+}
